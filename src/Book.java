115c167b7606479bcd52908023704cab3a10560a
Matheus Ribeiro <ribeiro7aith@gmail.com>
2026-06-11 23:12:22 -0300

    feat: add Book class with constructor, getters, setters and toString

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
-
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    public Book(String author, String isbn, boolean available, String title, int id) {
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+        this.title = title;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
